58da85f67917e0181f73de8b39940607eacafeab
BGYGIT <sd0678305003@gmail.com>
2026-02-24 18:36:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿function openRule(code, text, punishment) {
+    document.getElementById("modalText").innerHTML =
+        `<h2>${code}</h2>
+         <p>${text}</p>
+         <p><b>Покарання:</b> ${punishment}</p>`;
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
